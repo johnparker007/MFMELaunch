16ae1e066ce143f6e06528bb5d2dae38bea594f2
John Parker <gyrox007@gmail.com>
2023-08-19 10:49:41 +0100

    Initial hello world c# console app project

---
+++ MFMELaunch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MFMELaunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

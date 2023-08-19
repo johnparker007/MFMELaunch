fa3841ff342d5e3536da463d8ded9284545fd5f7
John Parker <gyrox007@gmail.com>
2023-08-19 12:18:33 +0100

    Now sending keypresses and successfully skipping popups
    All values hardcoded for now, minimal improvement will be to read at least the duration from a text file

--- MFMELaunch/Launcher.cs
+++ MFMELaunch/Launcher.cs
@@ -4,17 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Threading;
+using WindowsInput;
 
 namespace MFMELaunch
 {
     public class Launcher
     {
         private const string kMfmeExeFilename = "MFME.exe";
-        private const bool kWaitForMFMEExit = false;
+
+        private const float kKeypressesInitialDelay = 2.0f;
+        private const float kKeypressInterval = 0.05f;
+        private const float kKeypressesDuration = 9.0f;
 
         private string[] _args = null;
         private string _gamFilePath = null;
         private Process _mfmeProcess = null;
+
+        private InputSimulator _inputSimulator = null;
 
         public Launcher(string[] args)
         {
@@ -39,21 +46,45 @@ namespace MFMELaunch
 
             _gamFilePath = _args[0];
 
-            _mfmeProcess = new Process();
+            _mfmeProcess = CreateMFMEProcess();
 
-            _mfmeProcess.StartInfo.FileName = kMfmeExeFilename;
-            _mfmeProcess.StartInfo.Arguments = _gamFilePath;
-            _mfmeProcess.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
-            _mfmeProcess.Start();
-
-            // TODO various manipulations to send keys to skip popups etc...
-
-            if(kWaitForMFMEExit)
+            if(!_mfmeProcess.Start())
             {
-                _mfmeProcess.WaitForExit();
+                Console.WriteLine("Error starting MFME process");
+                return false;
             }
 
+            Thread.Sleep((int)(kKeypressesInitialDelay * 1000f));
+
+            SendKeyPresses(kKeypressInterval, kKeypressesDuration);
+
             return true;
+        }
+
+        private Process CreateMFMEProcess()
+        {
+            Process process = new Process();
+
+            process.StartInfo.FileName = kMfmeExeFilename;
+            process.StartInfo.Arguments = _gamFilePath;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+
+            return process;
+        }
+
+        private void SendKeyPresses(float pressInterval, float pressesDuration)
+        {
+            _inputSimulator = new InputSimulator();
+
+            int pressCount = (int)(pressesDuration / pressInterval);
+            for(int pressIndex = 0; pressIndex < pressCount; ++pressIndex)
+            {
+                Console.WriteLine("Generate keypress " + (pressIndex + 1) + " / " + pressCount);
+
+                _inputSimulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.SPACE);
+
+                Thread.Sleep((int)(pressInterval * 1000f));
+            }
         }
     }
 }
